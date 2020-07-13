37ae3c8fbb7ee371aa90ffdf7c1a9809f4f46a43
Gustavo Mattos <gusmattos1@SpeechGrammarList.com>
2020-07-12 21:01:53 -0300

    Includes PDF popup

---
+++ js/typing.js
@@ -0,0 +1,20 @@
+// Header
+
+var typed = new Typed('.typing', {
+    strings: ["UX Designer", "UI Designer", "Freelancer"],
+    loop: true,
+    typeSpeed: 40,
+    backSpeed: 40,
+    backDelay: 1500
+});
+
+
+//About Me
+
+var typed = new Typed('.about_typing', {
+    strings: ["UX Designer", "UI Designer", "Freelancer"],
+    loop: true,
+    typeSpeed: 40,
+    backSpeed: 40,
+    backDelay: 1500
+});
