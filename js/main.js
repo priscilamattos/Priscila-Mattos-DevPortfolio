275ee818fa790f257a006784c7ff3d074da222ee
Your Name <Priscila Mattos>
2022-04-21 00:37:35 -0400

    Portfolio section changes - html.

--- js/main.js
+++ js/main.js
@@ -1,48 +1,45 @@
 // MENU
 
-$('#close_menu').on('click', function(){
-    $('#close_menu').toggleClass('close__menu__right');
-    $('#close_menu').toggleClass('color');
-    $('#close_menu i').toggleClass('close__menu__rotate');
+$("#close_menu").on("click", function () {
+  $("#close_menu").toggleClass("close__menu__right");
+  $("#close_menu").toggleClass("color");
+  $("#close_menu i").toggleClass("close__menu__rotate");
 
-    $('#Menu').toggleClass('Menu_remove');
-    $('#header__content').toggleClass('header__content__full');
-    $('#content').toggleClass('header__content__full');
+  $("#Menu").toggleClass("Menu_remove");
+  $("#header__content").toggleClass("header__content__full");
+  $("#content").toggleClass("header__content__full");
 
-    $('#logo').toggleClass('opacity');
-    $('#nav').toggleClass('opacity');
-    $('#social').toggleClass('opacity');
-    
+  $("#logo").toggleClass("opacity");
+  $("#nav").toggleClass("opacity");
+  $("#social").toggleClass("opacity");
 });
-
 
 // MOB MENU
 
-
-$('#btn_nav').on('click', function(){
-    $('#mob_nav').toggleClass('mob_nav_open');
-    $('body').toggleClass('overflow_hidden');
-    $('.btn_nav span').toggleClass('btn_opacity');
-    $(this).toggleClass('btnNavActive');
+$("#btn_nav").on("click", function () {
+  $("#mob_nav").toggleClass("mob_nav_open");
+  $("body").toggleClass("overflow_hidden");
+  $(".btn_nav span").toggleClass("btn_opacity");
+  $(this).toggleClass("btnNavActive");
 });
 
-$('#mob_nav .nav a').on('click', function(){
-    $('.btn_nav').toggleClass('btnNavActive');
-    $('#mob_nav').toggleClass('mob_nav_open');
-    $('body').toggleClass('overflow_hidden');
+$("#mob_nav .nav a").on("click", function () {
+  $(".btn_nav").toggleClass("btnNavActive");
+  $("#mob_nav").toggleClass("mob_nav_open");
+  $("body").toggleClass("overflow_hidden");
 });
 
-$('.open-pdf-popup').magnificPopup({
-    type:'inline',
-    midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
-  });
+$(".open-pdf-popup").magnificPopup({
+  type: "inline",
+  midClick: true, // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+});
 
-$('#pdf').magnificPopup({
-    items: {
-        src: '#portifolio-2',
-        type: 'inline'
-    }
-  });
+$("#pdf").magnificPopup({
+  items: {
+    src: "#portifolio-2",
+    type: "inline",
+  },
+});
 // SCROLL TO LINK
 
 // $(document).ready(function(){
@@ -56,171 +53,93 @@ $('#pdf').magnificPopup({
 //     });
 // });
 
-
-// ANIMATION SKILLS
-
-$(document).ready(function(){
-
-    var show = true;
-    $(window).on("scroll", function(){
-
-        if(!show) return false;
-
-        var w_top = $(window).scrollTop();
-        var e_top = $("#about__content").offset().top;
-
-        if(w_top >= e_top){
-
-            $(".skillbar").each(function(){
-                $(this).find(".skill-bar").animate({
-                  width: $(this).attr("data-percent")
-                },2000)
-            });
-
-            // skills numbers
-
-            var time = 2;
-            $('#mySkills').each(function(){
-                $('label').each(function(){
-                    var
-                    i = 1,
-                    num = $(this).data('num'),
-                    step = 1000 * time / num,
-                    that = $(this),
-                    int = setInterval(function(){
-                        if(i <= num){
-                            that.html(i);
-                        }
-                        else{
-                            clearInterval(int);
-                        }
-                        i++;
-                    },step);
-                });
-            });
-
-            show = false;
-        }
-    });
-
-});
-
 //Happy Clients
 
+$(document).ready(function () {
+  var show = true;
+  $(window).on("scroll", function () {
+    if (!show) return false;
 
-$(document).ready(function(){
+    var w_top = $(window).scrollTop();
+    var e_top = $(".services__content .about__title h3").offset().top;
 
-    var show = true;
-    $(window).on("scroll", function(){
+    if (w_top >= e_top) {
+      var time = 2;
+      $("#counter").each(function () {
+        $("h4").each(function () {
+          var i = 1,
+            num = $(this).data("num"),
+            step = (1000 * time) / num,
+            that = $(this),
+            int = setInterval(function () {
+              if (i <= num) {
+                that.html(i);
+              } else {
+                clearInterval(int);
+              }
+              i++;
+            }, step);
+        });
+      });
 
-        if(!show) return false;
-
-        var w_top = $(window).scrollTop();
-        var e_top = $(".services__content .about__title h3").offset().top;
-
-        if(w_top >= e_top){
-
-            var time = 2;
-            $('#counter').each(function(){
-                $('h4').each(function(){
-                    var
-                    i = 1,
-                    num = $(this).data('num'),
-                    step = 1000 * time / num,
-                    that = $(this),
-                    int = setInterval(function(){
-                        if(i <= num){
-                            that.html(i);
-                        }
-                        else{
-                            clearInterval(int);
-                        }
-                        i++;
-                    },step);
-                });
-            });
-
-            show = false;
-        }
-    });
-
+      show = false;
+    }
+  });
 });
-
 
 // Filter Works
 
-
-$(document).ready(function(){
-    $(".btn_work").on("click", function(){
-        var value = $(this).attr("data-filter");
-        if(value == "all"){
-            $(".filter").show("1000");
-        }
-        else{
-            $(".filter").not("."+value).hide("1000");
-            $(".filter").filter("."+value).show("1000");
-        } 
-    });
-    // Add active class
-    $("ul .btn_work").click(function(){
-        $(this).addClass('active').siblings().removeClass('active');
-    });
+$(document).ready(function () {
+  $(".btn_work").on("click", function () {
+    var value = $(this).attr("data-filter");
+    if (value == "all") {
+      $(".filter").show("1000");
+    } else {
+      $(".filter")
+        .not("." + value)
+        .hide("1000");
+      $(".filter")
+        .filter("." + value)
+        .show("1000");
+    }
+  });
+  // Add active class
+  $("ul .btn_work").click(function () {
+    $(this).addClass("active").siblings().removeClass("active");
+  });
 });
 
 // GALLERY POPUP
 $(".open_galery").magnificPopup({
-    delegate: 'a',
-    type: 'image',
-    gallery:{
-        enabled: true
-    }
+  delegate: "a",
+  type: "image",
+  gallery: {
+    enabled: true,
+  },
 });
 // GALLERY POPUP END
 
-
-
-
-
-
-
-// FeedBack
-
-
-$(document).ready(function(){
-    $("#owl-carousel").owlCarousel({
-        items: 1,
-        loop: true,
-        autoplay: true,
-    });
-});
-
-
 // SCROLL TOP BUTTON
-$(document).ready(function(){
-    $(window).on("scroll", function(){
-
-        var w_top = $(window).scrollTop();
-        var e_top = $("#about__content").offset().top;
-        if(w_top >= e_top){
-            $(".scrollTop").css("opacity", "1");
-        }
-        if(w_top <= e_top){
-            $(".scrollTop").css("opacity", "0");
-        }
-    });
+$(document).ready(function () {
+  $(window).on("scroll", function () {
+    var w_top = $(window).scrollTop();
+    var e_top = $("#about__content").offset().top;
+    if (w_top >= e_top) {
+      $(".scrollTop").css("opacity", "1");
+    }
+    if (w_top <= e_top) {
+      $(".scrollTop").css("opacity", "0");
+    }
+  });
 });
 // SCROLL TOP BUTTON END
 
 // AJAX FORM
-$(document).ready(function() {
-
-
-
-});
+$(document).ready(function () {});
 // AJAX FORM END
 
 //MessageSent
-$("#SentClose").on("click", function(){
-    $("#MessageSent").fadeOut(500);
+$("#SentClose").on("click", function () {
+  $("#MessageSent").fadeOut(500);
 });
 //MessageSent END
