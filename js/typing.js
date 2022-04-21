2922b304f970432da7a2076290185a999c3ab187
Your Name <Priscila Mattos>
2022-04-20 22:38:53 -0400

    initial commit

--- js/typing.js
+++ js/typing.js
@@ -1,20 +1,19 @@
 // Header
 
-var typed = new Typed('.typing', {
-    strings: ["UX Designer", "UI Designer", "Instructional Designer"],
-    loop: true,
-    typeSpeed: 40,
-    backSpeed: 40,
-    backDelay: 1500
+var typed = new Typed(".typing", {
+  strings: ["Front-End Developer", "UX/UI Designer", "Instructional Designer"],
+  loop: true,
+  typeSpeed: 40,
+  backSpeed: 40,
+  backDelay: 1500,
 });
-
 
 //About Me
 
-var typed = new Typed('.about_typing', {
-    strings: ["UX Designer", "UI Designer", "Instructional Designer"],
-    loop: true,
-    typeSpeed: 40,
-    backSpeed: 40,
-    backDelay: 1500
+var typed = new Typed(".about_typing", {
+  strings: ["Front-End Developer", "UX/UI Designer", "Instructional Designer"],
+  loop: true,
+  typeSpeed: 40,
+  backSpeed: 40,
+  backDelay: 1500,
 });
